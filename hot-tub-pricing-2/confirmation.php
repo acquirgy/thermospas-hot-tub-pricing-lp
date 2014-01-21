3d7567b91bfea33f0f9900e9bee306c01acd5585
Cindy <crubin@acquirgy.com>
2014-01-21 17:24:34 -0500

    fix order of states

--- hot-tub-pricing-2/confirmation.php
+++ hot-tub-pricing-2/confirmation.php
@@ -106,57 +106,57 @@ if($_POST) {
                                         <div class="row-area state-wrapper">
                                             <select name="state" class="required-select state-wrapper row-area" title="State*" placeholder="State*">
                                                 <option value="0">State*</option>
-                                                <option>ID</option>
-                                                <option>IA</option>
                                                 <option>AL</option>
                                                 <option>AK</option>
                                                 <option>AZ</option>
                                                 <option>AR</option>
-                                                <option>WY</option>
-                                                <option>WA</option>
-                                                <option>VT</option>
-                                                <option>VA</option>
-                                                <option>WI</option>
-                                                <option>HI</option>
-                                                <option>DE</option>
-                                                <option>GA</option>
-                                                <option>WV</option>
-                                                <option>IL</option>
-                                                <option>IN</option>
-                                                <option>CA</option>
-                                                <option>KS</option>
-                                                <option>KY</option>
-                                                <option>CO</option>
+												<option>CA</option>
+												<option>CO</option>
                                                 <option>CT</option>
-                                                <option>LA</option>
-                                                <option>MA</option>
-                                                <option>MN</option>
+												<option>DE</option>
+												<option>FL</option>
+												<option>GA</option>
+												<option>HI</option>
+												<option>ID</option>
+                                                <option>IL</option>
+												<option>IN</option>
+												<option>IA</option>
+												<option>KS</option>
+                                                <option>KY</option>
+												<option>LA</option>
+												<option>ME</option>
+												<option>MD</option>
+												<option>MA</option>
+												<option>MI</option>
+												<option>MN</option>
                                                 <option>MS</option>
                                                 <option>MO</option>
-                                                <option>MI</option>
-                                                <option>MT</option>
-                                                <option>ME</option>
-                                                <option>MD</option>
-                                                <option>NE</option>
+												<option>MT</option>
+												<option>NE</option>
                                                 <option>NV</option>
                                                 <option>NH</option>
                                                 <option>NJ</option>
+												<option>NM</option>
                                                 <option>NY</option>
-                                                <option>NM</option>
-                                                <option>OH</option>
+												<option>NC</option>
+												<option>ND</option>
+												<option>OH</option>
                                                 <option>OK</option>
                                                 <option>OR</option>
                                                 <option>PA</option>
                                                 <option>RI</option>
-                                                <option>ND</option>
-                                                <option>NC</option>
-                                                <option>TN</option>
+												<option>SC</option>
+												<option>SD</option>
+												<option>TN</option>
                                                 <option>TX</option>
-                                                <option>FL</option>
-                                                <option>SD</option>
-                                                <option>SC</option>
-                                                <option>UT</option>
-                                            </select>
+												<option>UT</option>
+												<option>VT</option>
+												<option>VA</option>
+												<option>WA</option>
+												<option>WV</option>
+												<option>WI</option>
+                                                <option>WY</option>
+												</select>
                                         </div>
                                         <div class="row-area zip-wrapper">
                                             <input name="zipcode" class="required-number" type="text" placeholder="Zip*" title="Zip" />
